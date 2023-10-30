f2e92e91e96709750394abbdee148b4fabd998a6
신 우현 <woo_hyun_1@naver.com>
2023-10-30 23:16:20 +0900

    [Week4] Fetch course data from db

--- src/app/ultra/course/page.tsx
+++ src/app/ultra/course/page.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-const page = async () => {
+interface ICourse {
+  code: string;
+  name: string;
+  courseId: string;
+  professtorName: string;
+}
+
+const Course = () => {
+  const [courses, setCourses] = useState<ICourse[]>([]);
+  const getCourse = async () => {
+    const response = await fetch("http://localhost:9999/courses");
+    const courses = await response.json();
+    console.log(courses);
+    setCourses(courses);
+  };
+  useEffect(() => {
+    getCourse();
+  }, []);
   return (
     <div>
-      <span>코스 페이지 입니다 :)</span>
+      {courses.map((course) => (
+        <div>
+          <span>
+            {course.code}_ {course.name}({course.courseId})
+          </span>
+          <span>{course.professtorName}</span>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default page;
+export default Course;
